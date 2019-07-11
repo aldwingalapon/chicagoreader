ad36d7d78e533f10dbded1fb63d3d60e05d5f8ef
Aldwin Galapon <aldwin.galapon@gmail.com>
2019-07-12 02:11:15 +0800

    Message section implementation 021012072019

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php include $_SERVER['DOCUMENT_ROOT']."/chicagoreader/includes/header.php"; ?>
 
 	<div id="main-content" class="homepage index">
-		<div class="section">
+		<div class="section message">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-md-offset-1 col-md-10">
+						<p class="first"><span class="brace"><span class="highlight">Newly independent</span></span> with <span class="brace"><span class="highlight">new leadership</span></span>, the Chicago Reader hit the ground running in 2019. We've got <span class="brace"><span class="highlight">fresh talent</span></span> and <span class="brace"><span class="highlight">fresh energy</span></span>, and we're thrilled to continue to deliver our <span class="brace"><span class="highlight">unflagging</span></span>, <span class="brace"><span class="highlight">unfiltered</span></span> take on Chicago.</p>
+						<p class="last">We are Chicago's largest free, independent publication. We are Chicago's only alternative weekly. We are Chicago. And we speak Chicago.</p>
 					</div>
 					<div class="clearfix"></div>
 				</div>
