39e80137c5e59efdc487e1fce21b8f749a738068
Aldwin Galapon <aldwin.galapon@gmail.com>
2019-07-12 14:32:21 +0800

    Done with desktop implementation / for review 143212072019

--- index.php
+++ index.php
@@ -62,10 +62,23 @@
 		</div>
 		<div class="clearfix"></div>
 		
-		<div class="section">
-			<div class="container">
+		<div class="section yellow-people">
+			<div class="container-fluid">
+				<img src="images/reader-people-blank@3x.png" class="frame" alt="" title="" />
+			</div>
+			<div class="container yellow-people-inner">
 				<div class="row">
 					<div class="col-md-12">
+						<h3 class="black">Reader readers are diverse.</h3>
+						<ul class="circles">
+							<li><span class="circle"><span class="value">40%</span><span class="label">are Millennials</span></span></li>
+							<li><span class="circle"><span class="value">33.6%</span><span class="label">are Caucasian</span></span></li>
+							<li><span class="circle"><span class="value">30.6%</span><span class="label">are Latinx</span></span></li>
+							<li class="break"><span class="circle"><span class="value">25.3%</span><span class="label">are Black</span></span></li>
+							<li><span class="circle"><span class="value">9.6%</span><span class="label">are Asian</span></span></li>
+							<li><span class="circle"><span class="value">56%</span><span class="label">are female-identifying</span></span></li>
+							<li><span class="circle"><span class="value">20%</span><span class="label">identify as LGBTQ</span></span></li>
+						</ul>
 					</div>
 					<div class="clearfix"></div>
 				</div>
@@ -78,6 +91,10 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-offset-2 col-md-8">
+						<h3 class="black">They're Influential.</h3>
+						<p>Nearly half a million readers are active on social media</p>
+						<h3 class="yellow">And they've got money to spend.</h3>
+						<p>Nearly 40% earn more than $75k and nearly 10% earn more than $150k.</p>
 						<form>
 							<div class="description"><span class="clear">Find out more</span> <span class="clear">about how we can help you grow.</span> <span class="clear">Connect with us below to</span> <span class="clear">get our full media kit.</span></div>
 							<input type="text" placeholder="NAME" />
